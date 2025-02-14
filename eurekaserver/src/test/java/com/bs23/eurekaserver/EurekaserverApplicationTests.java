2fd70f8792d52cf102e299a55729bf3b6757cd65
sadikbs23 <sadik.hassan@brainstation-23.com>
2025-02-14 20:18:54 +0600

    upto eureka server setup

---
+++ eurekaserver/src/test/java/com/bs23/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bs23.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
